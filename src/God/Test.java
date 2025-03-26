2a990a46c1c6120a0c6e69bc47a2b25044f89632
Rakesh <rakeshiiet2010@gmail.com>
2025-03-27 03:33:57 +0530

    Third commit

---
+++ src/God/Test.java
@@ -0,0 +1,16 @@
+package God;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String s1 = new String("rakesh");
+		String s2 = new String("rakesh");
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
+		System.out.println("Hence 2 objects hashcode is same");;
+
+
+	}
+
+}
